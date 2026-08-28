9e023f9cbd99580da6662cbad790d14bffcf9002
Priyanka <ppriyankadtavare@gmail.com>
2026-08-28 11:30:21 +0530

    modified

--- name.py
+++ name.py
@@ -1,4 +1,6 @@
 name = input("Enter your name: ")
 surname = input("REnter your surname: ")
+contact = input("enter your contact details" )
 
 print("Your details are: ", name+" "+surname)
+print("your contact details is:"contact)
